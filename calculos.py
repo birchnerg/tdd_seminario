17cbff6426e6f0bbd6d190022f884a55a0484998
birchnerg <64251807+birchnerg@users.noreply.github.com>
2021-10-03 12:47:32 -0300

    Add files via upload

---
+++ calculos.py
@@ -0,0 +1,11 @@
+def suma (a, b):
+    return a + b
+
+assert suma(10, 5) == 15
+assert suma(-1, 18) == 17
+
+def multiplicacion (a, b):
+    pass
+
+assert multiplicacion(10, 5) == 50
+assert multiplicacion(-1, 18) == -18
