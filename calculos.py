1a618bf7ed45e97c282c2350df6fbcaad5c1d8e0
birchnerg <64251807+birchnerg@users.noreply.github.com>
2021-10-03 12:50:14 -0300

    Update calculos.py

--- calculos.py
+++ calculos.py
@@ -1,11 +1,11 @@
 def suma (a, b):
-    return a + b
+    pass
 
 assert suma(10, 5) == 15
 assert suma(-1, 18) == 17
 
 def multiplicacion (a, b):
-    pass
+    return a * b
 
 assert multiplicacion(10, 5) == 50
 assert multiplicacion(-1, 18) == -18
